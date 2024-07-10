77621a16e9ee85b5d56a7fcc3ac3e44c22f654b6
nguyenhoanghiep478 <97501314+nguyenhoanghiep478@users.noreply.github.com>
2024-07-11 00:43:39 +0700

    Delete src/main/java/com/example/bankapi/ExceptionHandle directory

--- src/main/java/com/example/bankapi/ExceptionHandle/InsufficientBalanceException.java
+++
@@ -1,7 +0,0 @@
-package com.example.bankapi.ExceptionHandle;
-
-public class InsufficientBalanceException extends RuntimeException{
-    public InsufficientBalanceException(String message){
-        super(message);
-    }
-}
