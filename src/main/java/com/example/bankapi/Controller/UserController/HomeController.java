eedefcf9217e8a3cfd112908ea5542199ab0aaed
nguyenhoanghiep478 <97501314+nguyenhoanghiep478@users.noreply.github.com>
2024-07-11 00:46:11 +0700

    Update HomeController.java

--- src/main/java/com/example/bankapi/Controller/UserController/HomeController.java
+++ src/main/java/com/example/bankapi/Controller/UserController/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.bankapi.Controller.UserController;
 
-<<<<<<< HEAD
 import com.example.bankapi.DTO.Email.EmailRequest;
 import com.example.bankapi.DTO.Email.VerificationEmailRequest;
 import com.example.bankapi.Service.Email.IEmailService;
@@ -13,28 +12,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.function.EntityResponse;
-=======
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
->>>>>>> 8bf2d517198aeb5b5c93bc71d0821bb6b2eddbb3
+
 
 @RestController
 @Slf4j
 @Tag(name="User Controller")//gán tên cho swagger
-<<<<<<< HEAD
 @RequiredArgsConstructor
 public class HomeController {
-
-=======
-@RequestMapping("/public")
-public class HomeController {
->>>>>>> 8bf2d517198aeb5b5c93bc71d0821bb6b2eddbb3
     @GetMapping("/home")
     public String home(){
         Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
@@ -49,8 +33,4 @@ public class HomeController {
         authentication.getAuthorities().forEach(s-> log.info(s.getAuthority()));
         return "HEllO THERE";
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> 8bf2d517198aeb5b5c93bc71d0821bb6b2eddbb3
 }
