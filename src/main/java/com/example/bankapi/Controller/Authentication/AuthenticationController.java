aa566e6a0246afc858b033fd596c772cb832d6c6
nguyenhoanghiep478 <97501314+nguyenhoanghiep478@users.noreply.github.com>
2024-07-11 00:51:23 +0700

    Update AuthenticationController.java

--- src/main/java/com/example/bankapi/Controller/Authentication/AuthenticationController.java
+++ src/main/java/com/example/bankapi/Controller/Authentication/AuthenticationController.java
@@ -6,12 +6,8 @@ import com.example.bankapi.DTO.Authentication.RegisterRequest;
 import com.example.bankapi.DTO.Authentication.RegisterResponse;
 import com.example.bankapi.DTO.Email.EmailRequest;
 import com.example.bankapi.DTO.Email.VerificationEmailRequest;
-<<<<<<< HEAD
 import com.example.bankapi.Service.Authentication.IAuthenticationService;
 import com.example.bankapi.Service.Authentication.impl.AuthenticationServiceImpl;
-=======
-import com.example.bankapi.Service.Authentication.AuthenticationService;
->>>>>>> 8bf2d517198aeb5b5c93bc71d0821bb6b2eddbb3
 import com.example.bankapi.Service.Email.IEmailService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,11 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @SecurityRequirement(name="")
 @Tag(name="Authentication Controller")//gán tên cho swagger
 public class AuthenticationController {
-<<<<<<< HEAD
     private final IAuthenticationService service;
-=======
-    private final AuthenticationService service;
->>>>>>> 8bf2d517198aeb5b5c93bc71d0821bb6b2eddbb3
     private final IEmailService emailService;
     @PostMapping ("/register")
     public ResponseEntity<RegisterResponse> register(@RequestBody RegisterRequest request){
