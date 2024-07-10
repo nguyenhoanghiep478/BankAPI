5eac964b134255d1d16cfdc0c856c956549129cc
nguyenhoanghiep478 <97501314+nguyenhoanghiep478@users.noreply.github.com>
2024-07-11 00:50:20 +0700

    Update RegisterResponse.java

--- src/main/java/com/example/bankapi/DTO/Authentication/RegisterResponse.java
+++ src/main/java/com/example/bankapi/DTO/Authentication/RegisterResponse.java
@@ -11,8 +11,4 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterResponse {
     private String email;
-<<<<<<< HEAD
-=======
-    private String token;
->>>>>>> 8bf2d517198aeb5b5c93bc71d0821bb6b2eddbb3
 }
