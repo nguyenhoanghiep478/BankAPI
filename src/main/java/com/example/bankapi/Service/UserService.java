612db71576f505805f19fd0e1b44df85feffcfe9
nguyenhoanghiep478 <97501314+nguyenhoanghiep478@users.noreply.github.com>
2024-07-11 00:52:37 +0700

    Update UserService.java

--- src/main/java/com/example/bankapi/Service/UserService.java
+++ src/main/java/com/example/bankapi/Service/UserService.java
@@ -1,10 +1,6 @@
 package com.example.bankapi.Service;
 
-<<<<<<< HEAD
 import com.example.bankapi.Repositories.Authentication.User;
-=======
-import com.example.bankapi.Repositories.User;
->>>>>>> 8bf2d517198aeb5b5c93bc71d0821bb6b2eddbb3
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -14,11 +10,9 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final User repository;
     private final ModelMapper modelMapper;
-
-<<<<<<< HEAD
+    
     public com.example.bankapi.Entity.Authentication.User findByEmail(String userEmail) {
         return repository.findByEmail(userEmail);
     }
-=======
->>>>>>> 8bf2d517198aeb5b5c93bc71d0821bb6b2eddbb3
+
 }
